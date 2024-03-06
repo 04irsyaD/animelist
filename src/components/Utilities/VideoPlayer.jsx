c08663c938851d354c1ede3a74c5a82a5b490e69
irsyadrisno <irsyadrisno@gmail.com>
2024-03-06 16:24:15 +0700

    fix trailer

--- src/components/Utilities/VideoPlayer.jsx
+++ src/components/Utilities/VideoPlayer.jsx
@@ -1,22 +1,54 @@
 "use client"
 
 import Youtube from "react-youtube"
-import {  XCircle } from "@phosphor-icons/react"
+import { useState } from "react"
+
+
 
 const VideoPlayer = ({ youtubeId }) => {
+    const [isOpen, setIsOpen] = useState(true)
+
+    const handleVideoPlayer = () => {
+        setIsOpen((prevState) => !prevState)
+    }
+
     const option = {
         width: "300",
-        height:"250"
+        height: "250"
     }
-    return (
-        <div className="fixed bottom-0 right-0">
-            <button><XCircle size={32}/></button>
-            <Youtube videoId={youtubeId} 
-            onReady={(event) => event.target.pauseVideo() }
-            opts={option}
-            />
-        </div>
-    )
+
+    const Player = () => {
+        return (
+            <div className="fixed bottom-2 right-2">
+                <button
+                    onClick={handleVideoPlayer}
+                    className="text-color-primary float-right bg-color-secondary px-3 mb-1">
+                    X
+                </button>
+                <Youtube videoId={youtubeId}
+                    onReady={(event) => event.target.pauseVideo()}
+                    opts={option}
+                />
+            </div>
+        )
+    }
+
+    const ButtonOpenPlayer = () => {
+        return (
+            
+                <button
+                    onClick={handleVideoPlayer}
+                    className="fixed bottom-5 right-5 w-32 bg-color-primary text-color-dark text-xl hover:bg-color-accent transition-all shadow-xl">
+                    Tonton Trailer
+                </button>
+        )
+    }
+
+
+    return isOpen ? <Player /> : <ButtonOpenPlayer/>
+
+
+
 }
 
 export default VideoPlayer
